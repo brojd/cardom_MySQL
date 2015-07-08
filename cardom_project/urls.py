e4b25e79eda792a550c304583b65740716d33feb
dominik791 <dominik.broj@gmail.com>
2015-07-08 19:49:37 +0200

    set static media server

--- cardom_project/urls.py
+++ cardom_project/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+
 
 urlpatterns = [
     # Examples:
@@ -8,3 +10,7 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static', (r'^media/(?P<path>.*)', 'serve', {'document_root': settings.MEDIA_ROOT}),)
