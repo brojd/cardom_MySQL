208887df222919d114c28ea714fea44160193753
dominik791 <dominik.broj@gmail.com>
2015-07-12 15:13:40 +0200

    Improve OfferFilter to use RangeFilter()

--- cardom/filters.py
+++ cardom/filters.py
@@ -2,6 +2,10 @@ import django_filters
 from .models import Offer
 
 class OfferFilter(django_filters.FilterSet):
+    floor_space = django_filters.RangeFilter(label='Powierzchnia [m2]')
+    price = django_filters.RangeFilter(label='Cena')
+    nb_rooms = django_filters.RangeFilter(label='Liczba pokoi')
+    
     class Meta:
         model = Offer
-        fields = ['category', 'province', 'balcony']
+        fields = ['transaction', 'category', 'province', 'city',  'nb_rooms', 'floor_space', 'price']
