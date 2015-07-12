5a1a1b2339577ba1d05a5717d3be61a9ea929274
dominik791 <dominik.broj@gmail.com>
2015-07-12 14:21:07 +0200

    provide search box on every page

--- cardom/views.py
+++ cardom/views.py
@@ -8,49 +8,77 @@ def index(request):
     category_list = Category.objects.all
     latest_offers = Offer.objects.order_by('-pub_date')[:10]
     promoted_offers = Offer.objects.filter(promoted=True).order_by('-pub_date')[:10]
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
     
-    context_dict = {'categories': category_list,
-                    'latest_offers': latest_offers,
-                    'promoted_offers': promoted_offers}
+    context_dict = {
+        'categories': category_list,
+        'latest_offers': latest_offers,
+        'promoted_offers': promoted_offers,
+        'filter': f,
+                    }
     
     return render(request, 'cardom/index.html', context_dict)
 
 def flats(request):
     flat_offers = Offer.objects.filter(category__name='M').order_by('-pub_date')
-    context_dict = {'flat_offers': flat_offers}
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    context_dict = {
+        'flat_offers': flat_offers,
+        'filter': f
+        }
     return render(request, 'cardom/flats.html', context_dict)
 
 def houses(request):
     house_offers = Offer.objects.filter(category__name='D').order_by('-pub_date')
-    context_dict = {'house_offers': house_offers}
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    context_dict = {
+        'house_offers': house_offers,
+        'filter': f,
+        }
     return render(request, 'cardom/houses.html', context_dict)
 
 def lots(request):
     lot_offers = Offer.objects.filter(category__name='DZ').order_by('-pub_date')
-    context_dict = {'lot_offers': lot_offers}
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    context_dict = {
+        'lot_offers': lot_offers,
+        'filter': f,
+        }
     return render(request, 'cardom/lots.html', context_dict)
 
 def locals(request):
     local_offers = Offer.objects.filter(category__name='L').order_by('-pub_date')
-    context_dict = {'local_offers': local_offers}
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    context_dict = {
+        'local_offers': local_offers,
+        'filter': f,
+        }
     return render(request, 'cardom/locals.html', context_dict)
 
 def other_objects(request):
     other_object_offers = Offer.objects.filter(category__name='O').order_by('-pub_date')
-    context_dict = {'other_object_offers': other_object_offers}
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    context_dict = {
+        'other_object_offers': other_object_offers,
+        'filter': f,
+                    }
     return render(request, 'cardom/other_objects.html', context_dict)
 
 def about(request):
-    return render(request, 'cardom/about.html', {})
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())    
+    return render(request, 'cardom/about.html', {'filter': f})
 
 def contact(request):
-    return render(request, 'cardom/contact.html', {})
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    return render(request, 'cardom/contact.html', {'filter': f})
 
 def career(request):
-    return render(request, 'cardom/career.html', {})
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    return render(request, 'cardom/career.html', {'filter': f})
 
 def publish_offer(request):
-    return render(request, 'publish_offer.html', {})
+    f = OfferFilter(request.GET, queryset=Offer.objects.all())
+    return render(request, 'cardom/publish_offer.html', {'filter': f})
 
 def offer_list(request):
     f = OfferFilter(request.GET, queryset=Offer.objects.all())
--- cardom_project/settings.py
+++ cardom_project/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cardom',
-    'filters',
+    'django_filters',
 )
 
 MIDDLEWARE_CLASSES = (
