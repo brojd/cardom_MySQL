13d5e623554966c19f55ae44bc82a0b8cfeef83f
dominik791 <dominik.broj@gmail.com>
2015-07-15 20:39:03 +0200

    bootstraping base.html

--- cardom_project/urls.py
+++ cardom_project/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import patterns
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Examples:
@@ -18,3 +19,6 @@ if settings.DEBUG:
         (r'^media/(?P<path>.*)',
          'serve',
          {'document_root': settings.MEDIA_ROOT}), )
+    
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
