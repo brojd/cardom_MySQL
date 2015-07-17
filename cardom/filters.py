83260bf697dde92bd1b82aed2ad131b56538c6a1
dominik791 <dominik.broj@gmail.com>
2015-07-17 21:39:36 +0200

    edit searchbox

--- cardom/filters.py
+++ cardom/filters.py
@@ -2,10 +2,17 @@ import django_filters
 from .models import Offer
 
 class OfferFilter(django_filters.FilterSet):
-    floor_space = django_filters.RangeFilter(label='Powierzchnia [m2]')
-    price = django_filters.RangeFilter(label='Cena')
-    nb_rooms = django_filters.RangeFilter(label='Liczba pokoi')
+    min_floor_space = django_filters.NumberFilter(lookup_type='gte')
+    max_floor_space = django_filters.NumberFilter(lookup_type='lte')
+    min_price = django_filters.NumberFilter(lookup_type='gte')
+    max_price = django_filters.NumberFilter(lookup_type='lte')
+    min_nb_rooms = django_filters.NumberFilter(lookup_type='gte')
+    max_nb_rooms = django_filters.NumberFilter(lookup_type='lte')
     
     class Meta:
         model = Offer
-        fields = ['transaction', 'category', 'province', 'city',  'nb_rooms', 'floor_space', 'price']
+        fields = [
+            'transaction', 'category', 'province', 'city',
+            'nb_rooms', 'min_floor_space', 'max_floor_space',
+            'min_price', 'max_price', 'min_nb_rooms', 'max_nb_rooms'
+            ]
